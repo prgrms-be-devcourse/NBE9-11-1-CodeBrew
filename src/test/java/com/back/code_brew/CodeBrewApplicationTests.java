95004558f3e9e4748ee2233eeeabd132b1fb4675
gmltmdu <apple@appleui-MacBookPro.local>
2026-03-19 16:31:39 +0900

    init

---
+++ src/test/java/com/back/code_brew/CodeBrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.code_brew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
