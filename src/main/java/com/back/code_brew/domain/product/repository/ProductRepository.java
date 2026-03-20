05802a8e8f024f65a0c451fd0404e20a65c31ac5
karo119 <jsin991019@gmail.com>
2026-03-20 12:21:55 +0900

    feat: product CRUD 구현 및 ì´initData 추가

---
+++ src/main/java/com/back/code_brew/domain/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.back.code_brew.domain.product.repository;
+
+import com.back.code_brew.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
---
+++ src/main/java/com/back/code_brew/global/initData/BaseInitData.java
@@ -0,0 +1,32 @@
+package com.back.code_brew.global.initData;
+
+import com.back.code_brew.domain.product.entity.Product;
+import com.back.code_brew.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BaseInitData {
+
+    private final ProductService productService;
+
+    @Bean
+    public ApplicationRunner initData() {
+        return args -> work1();
+    }
+
+    public void work1() {
+        if(productService.count() > 0){
+            return;
+        }
+        Product product1 = productService.create("Ethiopia Sidamo", 5000);
+        Product product2 = productService.create("Columbia Quindio", 3000);
+        Product product3 = productService.create("Brazil Serra Do Caparao", 7000);
+        Product product4 = productService.create("Columbia Narino", 6500);
+
+    }
+
+}
